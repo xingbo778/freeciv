69e5b2e641f6735eece1de6fa8a06d91d218d450
Marko Lindqvist <cazfi74@gmail.com>
2024-08-31 14:47:00 +0300

    Rename action_..._unit_units as action_..._unit_stack
    See RM #817
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/unithand.c
+++ server/unithand.c
@@ -3566,6 +3566,8 @@ bool unit_perform_action(struct player *pplayer,
       && is_action_enabled_unit_on_stack(nmap, action_type,               \
                                          actor_unit, target_tile)) {      \
     bool success;                                                         \
+    script_server_signal_emit("action_started_unit_stack",                \
+                              action_by_number(action), actor, target);   \
     script_server_signal_emit("action_started_unit_units",                \
                               action_by_number(action), actor, target);   \
     if (!actor || !unit_is_alive(actor_id)) {                             \
@@ -3576,6 +3578,10 @@ bool unit_perform_action(struct player *pplayer,
     if (success) {                                                        \
       action_success_actor_price(paction, actor_id, actor);               \
     }                                                                     \
+    script_server_signal_emit("action_finished_unit_stack",               \
+                              action_by_number(action), success,          \
+                              unit_is_alive(actor_id) ? actor : NULL,     \
+                              target);                                    \
     script_server_signal_emit("action_finished_unit_units",               \
                               action_by_number(action), success,          \
                               unit_is_alive(actor_id) ? actor : NULL,     \
