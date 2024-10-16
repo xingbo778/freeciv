0ac225897eaefdc50ac6549783d1a570741e748f
Marko Lindqvist <cazfi74@gmail.com>
2024-10-16 04:54:57 +0300

    Rename Martial_Law_Each as Martial_Law_By_Unit
    See RM #972
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- common/city.c
+++ common/city.c
@@ -3149,7 +3149,7 @@ static inline void city_support(struct city *pcity)
                                                  .unit = punit,
                                                  .unittype = unit_type_get(punit)
                                                },
-                                               NULL, EFT_MARTIAL_LAW_EACH);
+                                               NULL, EFT_MARTIAL_LAW_BY_UNIT);
         if (current > 0) {
           if (sel_count < max_mart_units) {
             best_units[sel_count++] = current;
