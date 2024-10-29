572eb5abc4edb16d12ea94c3c32281c7dcbb854c
Marko Lindqvist <cazfi74@gmail.com>
2024-10-29 23:39:49 +0200

    Replace civstyle civil war bonus values with Civil_War_City_Bonus effects
    See RM #1032
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- tools/ruleutil/rulesave.c
+++ tools/ruleutil/rulesave.c
@@ -1377,12 +1377,6 @@ static bool save_game_ruleset(const char *filename, const char *name)
   save_default_bool(sfile, game.info.civil_war_enabled,
                     TRUE,
                     "civstyle.civil_war_enabled", NULL);
-  save_default_int(sfile, game.info.civil_war_bonus_celebrating,
-                   RS_DEFAULT_CIVIL_WAR_CELEB,
-                   "civstyle.civil_war_bonus_celebrating", NULL);
-  save_default_int(sfile, game.info.civil_war_bonus_unhappy,
-                   RS_DEFAULT_CIVIL_WAR_UNHAPPY,
-                   "civstyle.civil_war_bonus_unhappy", NULL);
   save_default_bool(sfile,
                     BV_ISSET(action_by_number(ACTION_PARADROP)->sub_results,
                              ACT_SUB_RES_MAY_EMBARK),
