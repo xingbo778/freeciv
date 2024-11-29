26937323ede3a59f8206bfa94be83413f6936074
Marko Lindqvist <cazfi74@gmail.com>
2024-11-30 01:47:14 +0200

    rscompat: Configure action by old ui_name only if non-default
    See RM #1168
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/ruleset/rscompat.c
+++ server/ruleset/rscompat.c
@@ -775,7 +775,9 @@ bool load_action_ui_name_3_3(struct section_file *file, int act,
     if (text == NULL) {
       text = def;
     } else {
-      paction->configured = TRUE;
+      if (strcmp(def, text)) {
+        paction->configured = TRUE;
+      }
     }
   }
 
