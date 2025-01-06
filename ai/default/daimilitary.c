dde3db4c2069fee92de8d816bcd2ed6a5854988f
Marko Lindqvist <cazfi74@gmail.com>
2025-01-06 08:10:26 +0200

    AI: Pass civ_map to dai_choose_attacker_air()
    See RM #1205
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- ai/default/daimilitary.c
+++ ai/default/daimilitary.c
@@ -2012,7 +2012,8 @@ struct adv_choice *military_advisor_choose_build(struct ai_type *ait,
   }
 
   /* Consider making an airplane */
-  (void) dai_choose_attacker_air(ait, pplayer, pcity, choice, allow_gold_upkeep);
+  (void) dai_choose_attacker_air(ait, nmap, pplayer, pcity, choice,
+                                 allow_gold_upkeep);
 
   /* Consider making a paratrooper */
   dai_choose_paratrooper(ait, nmap, pplayer, pcity, choice, allow_gold_upkeep);
