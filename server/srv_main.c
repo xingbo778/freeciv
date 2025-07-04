c8b7da8174d74c2a29318f8832a533de8d26b11d
Marko Lindqvist <cazfi74@gmail.com>
2025-07-04 23:41:52 +0300

    Accept PACKET_SYNC_SERIAL from non-player connections
    See RM #1562
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/srv_main.c
+++ server/srv_main.c
@@ -2094,7 +2094,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
 {
   struct player *pplayer;
 
-  /* a NULL packet can be returned from receive_packet_goto_route() */
+  /* A NULL packet can be returned from receive_packet_goto_route() */
   if (!packet) {
     return TRUE;
   }
@@ -2163,7 +2163,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
     return TRUE;
   }
 
-  /* valid packets from established connections but non-players */
+  /* Valid packets from established connections but non-players */
   if (type == PACKET_CHAT_MSG_REQ
       || type == PACKET_SINGLE_WANT_HACK_REQ
       || type == PACKET_NATION_SELECT_REQ
@@ -2172,6 +2172,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
       || type == PACKET_CONN_PONG
       || type == PACKET_CLIENT_HEARTBEAT
       || type == PACKET_SAVE_SCENARIO
+      || type == PACKET_SYNC_SERIAL
       || is_client_edit_packet(type)) {
 
     /* Except for PACKET_EDIT_MODE (used to set edit mode), check
@@ -2197,7 +2198,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
       handle_observer_ready(pconn);
       return TRUE;
     }
-    /* don't support these yet */
+    /* Don't support these yet */
     log_error("Received packet %s(%d) from non-player connection %s.",
               packet_name(type), type, conn_description(pconn));
     return TRUE;
@@ -2246,6 +2247,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
   }
 
   pplayer->current_conn = NULL;
+
   return TRUE;
 }
 
