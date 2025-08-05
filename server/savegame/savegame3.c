dc2abc46ee77b4e0e9a5247dceeaf0522c12b318
John Robertson <jro@advops.com>
2025-08-04 22:44:17 -0600

    savegame: Fix crash with illegal action lists
    During the save or load of a game, if an order's action list is unapproved,
    or malformed, the internal memory for the action list is deleted and
    set to NULL.
    
    But the deleted memory is later accessed as part of a sanity test.
    
    Added defensive code to guard against access of the NULL pointer.
    
    See RM #1646

--- server/savegame/savegame3.c
+++ server/savegame/savegame3.c
@@ -6505,7 +6505,9 @@ static bool sg_load_player_unit(struct loaddata *loading,
           /* An invalid order. Just drop the orders for this unit. */
           free(punit->orders.list);
           punit->orders.list = NULL;
+          punit->orders.length = 0;
           punit->has_orders = FALSE;
+          punit->goto_tile = NULL;
           break;
         }
 
@@ -6592,7 +6594,9 @@ static bool sg_load_player_unit(struct loaddata *loading,
             /* Missing required action extra target. */
             free(punit->orders.list);
             punit->orders.list = NULL;
+            punit->orders.length = 0;
             punit->has_orders = FALSE;
+            punit->goto_tile = NULL;
             break;
           }
         } else if (order->order != ORDER_PERFORM_ACTION) {
@@ -6616,7 +6620,9 @@ static bool sg_load_player_unit(struct loaddata *loading,
       int j;
 
       punit->has_orders = FALSE;
+      punit->goto_tile = NULL;
       punit->orders.list = NULL;
+      punit->orders.length = 0;
 
       (void) secfile_entry_lookup(loading->file, "%s.orders_index", unitstr);
       (void) secfile_entry_lookup(loading->file, "%s.orders_repeat", unitstr);
@@ -8168,8 +8174,9 @@ static void sg_load_sanitycheck(struct loaddata *loading)
 
   players_iterate(pplayer) {
     unit_list_iterate_safe(pplayer->units, punit) {
-      if (!unit_order_list_is_sane(&(wld.map), punit->orders.length,
-                                   punit->orders.list)) {
+      if (punit->has_orders
+          && !unit_order_list_is_sane(&(wld.map), punit->orders.length,
+                                      punit->orders.list)) {
         log_sg("Invalid unit orders for unit %d.", punit->id);
         free_unit_orders(punit);
       }
