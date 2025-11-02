d568d07a4a8f0898b09801153baa97e7df244b2e
Marko Lindqvist <cazfi74@gmail.com>
2025-11-02 22:49:23 +0200

    Drop empty stdinhand_turn()
    See RM #1510
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- server/srv_main.c
+++ server/srv_main.c
@@ -1812,7 +1812,6 @@ static void end_turn(void)
   update_diplomatics();
   make_history_report();
   settings_turn();
-  stdinhand_turn();
   voting_turn();
   send_city_turn_notifications(NULL);
 
